4ddb107a0d570b24a77b5d7283128aee5b409bb5
Iksan Maricar <iksansahib@gmail.com>
2018-02-18 18:57:42 +0700

    add post /todos and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var newTodo = new Todo({
+    text: req.body.text
+  });
+
+  newTodo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('start');
+});
