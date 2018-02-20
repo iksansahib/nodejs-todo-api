f9b165a6e7c319d90bd9d0def8a884ee175d5144
Iksan Maricar <iksansahib@gmail.com>
2018-02-20 23:38:13 +0700

    fix undefined port

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var {ObjectID} = require('mongodb');
 var app = express();
 
 //const port = 3000;
-const port = process.ENV.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
