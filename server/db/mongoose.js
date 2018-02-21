d40834fcd7e4377a0738e7bde01408c18955bfb6
Iksan Maricar <iksansahib@gmail.com>
2018-02-21 23:20:00 +0700

    separate env for test and dev

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 
 //mongoose.connect('mongodb://iksansahib:diahcitra85@ds243728.mlab.com:43728/todoapp');
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 module.exports = {mongoose};
